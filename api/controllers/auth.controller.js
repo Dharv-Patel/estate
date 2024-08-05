758e809ed84d729e7fd94293922b0c830d216b52
Dharv-Patel <dharvpatel8524@gmail.com>
2024-08-05 21:46:53 +0530

    modify signup api - handle error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -9,6 +9,6 @@ export const signup = async (req,res)=>{
         await newUser.save()
         res.status(201).json(newUser)
     } catch (error) {
-        res.status(500).json(error)
+        res.status(500).json(error.message)
     }
 }
