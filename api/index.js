7a326bb370bc681c367613e7f050c662a6901a6f
Dharv-Patel <dharvpatel8524@gmail.com>
2024-08-05 18:34:48 +0530

    Add mongoDB connection

--- api/index.js
+++ api/index.js
@@ -1,6 +1,19 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+dotenv.config();  // use it for env variable
+
+
+mongoose
+    .connect(process.env.MONGO)
+    .then(()=>{
+        console.log('DataBase Connected!');
+    })
+    .catch((err)=>{
+    console.log(err);
+})
 
 const app = express()
 
 
-app.listen(9000,()=>console.log('server is started!'))
+app.listen(9000,()=>console.log('server is running on port : \'9000\'!'))
