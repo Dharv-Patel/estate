171a6f1dc6866e9f615e214a7dded3911d5f8961
Dharv-Patel <dharvpatel8524@gmail.com>
2024-08-06 22:32:02 +0530

    add signin api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
+import { handelError } from "../utils/error.js";
 import bcryptjs from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
 export const signup = async (req,res,next)=>{
     try {
@@ -8,6 +10,24 @@ export const signup = async (req,res,next)=>{
         const newUser = new User({username, email, password: haspassword})
         await newUser.save()
         res.status(201).json("user add sucessfull")
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const signin = async (req,res,next)=>{
+    try {
+        const {email,password} = req.body;
+        const userData = await User.findOne({email})
+        if(!userData)
+            return next(handelError(404,'User Not Found'))
+        const validPassword = await bcryptjs.compareSync(password,userData.password)
+        if(!validPassword)
+            return next(handelError(401,'Password is not correct'))
+        const token = jwt.sign({id:userData._id},process.env.JWT_SECRET)
+
+        const {password:pass,...rest} = userData._doc;
+        res.cookie('Access_token',token,{httpOnly: true}).status(200).json(rest);
     } catch (error) {
         next(error)
     }
